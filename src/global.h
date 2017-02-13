572ad916a3e33ec0985ece6f652161543fcb0661
Ulf Lehnert <lehnertu@users.noreply.github.com>
2017-02-13 14:57:21 +0100

    Update global.h

--- src/global.h
+++ src/global.h
@@ -7,7 +7,7 @@
 /************************************************************************/
 
 #define ElementaryCharge 1.60203e-19            // [As]
-#define mecsquared 0.511048e6                   // electron mass [eV]
+#define mecsquared 0.5109989461e6               // electron mass [eV]
 #define SpeedOfLight 2.99792458e8               // [m/s]
 #define Pi 3.1415926535897932384626433832795029
 #define EpsNull 8.854187817e-12                 // vacuum permittivity [As/Vm] 1/c² = e0*µ0
