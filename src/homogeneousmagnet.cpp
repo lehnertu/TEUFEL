c763149d915a06eaba8dfc72a73dd5665c51cc31
Ulf Lehnert <ulf@lehnertu.de>
2017-02-14 15:07:46 +0100

    cleanup file structure

---
+++ src/homogeneousmagnet.cpp
@@ -0,0 +1,29 @@
+/************************************************************************/
+/*                                                                      */
+/*  TEUFEL - THz Emission from Undulators and Free-Electron Lasers      */
+/*                                                                      */
+/*  written by  U.Lehnert & Vipul Joshi                 12/2016         */
+/*                                                                      */
+/************************************************************************/
+
+#include "homogeneousmagnet.h"
+#include "global.h"
+#include <math.h>
+
+HomogeneousMagnet::HomogeneousMagnet(Vector B)
+{
+  B0=B;
+}
+
+Vector HomogeneousMagnet::ElementLocalEField(double t, Vector X)
+{
+  Vector p=Vector(0.0,0.0,0.0);
+  return (p);
+}
+
+Vector HomogeneousMagnet::ElementLocalBField(double t, Vector X)
+{
+  return B0;
+}
+
+
---
+++ src/homogeneousmagnet.h
@@ -0,0 +1,34 @@
+#ifndef HOMOGENEOUSMAGNET_H
+#define HOMOGENEOUSMAGNET_H
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "externalfield.h"
+#include "vector.h"
+
+using namespace std;
+
+//a class for a homogenous magnetic field 
+//the field would extend everywhere
+
+class HomogeneousMagnet : public ExternalField
+{
+
+  public:
+
+    // constructor
+    HomogeneousMagnet(Vector B);              // field vector B[T]
+
+  private:
+
+    Vector ElementLocalEField(double t, Vector X);
+
+    Vector ElementLocalBField(double t, Vector X);
+
+  public:
+      
+    Vector B0;				// the constant field vector
+};
+
+#endif
