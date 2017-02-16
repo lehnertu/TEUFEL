b381daa845c16ca159d28b102debfb49ec2cea71
Ulf Lehnert <ulf@lehnertu.de>
2017-02-16 11:18:59 +0100

    fixed preamble of the source files

--- src/wave.h
+++ src/wave.h
@@ -1,10 +1,23 @@
-/************************************************************************/
-/*                                                                      */
-/*  TEUFEL - THz Emission from Undulators and Free-Electron Lasers      */
-/*                                                                      */
-/*  written by  U.Lehnert                               12/2016         */
-/*                                                                      */
-/************************************************************************/
+/*=========================================================================
+ * 
+ *  Program:   TEUFEL - THz Emission from Undulators and Free-Electron Lasers
+ * 
+ *  Copyright (c) 2017 U. Lehnert
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * =========================================================================*/
 
 #ifndef WAVE_H
 #define WAVE_H
