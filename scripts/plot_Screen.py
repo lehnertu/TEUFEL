f6eb52e3358d06264302843eb9817c1921f2457c
Ulf Lehnert <ulf@lehnertu.de>
2018-04-12 16:00:21 +0200

    update plotting scripts for screen output

--- scripts/plot_Screen.py
+++ scripts/plot_Screen.py
@@ -15,9 +15,9 @@ mu0 = 4*np.pi*1e-7
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', help='the file name of the watch point HDF5 file')
-parser.add_argument('--maxf', help="plot range for the spectrum in Hz", dest="maxf", type=float)
-parser.add_argument('--ROI', help="ROI for the spectrum in Hz", dest="roi", type=float, nargs=2)
-parser.add_argument('--circ', help="ROI for the spatial dist.", dest="circ", type=float)
+parser.add_argument('-fmax', help="plot range for the spectrum in Hz", dest="fmax", type=float)
+parser.add_argument('-roi', help="ROI for the spectrum in Hz", dest="roi", type=float, nargs=2)
+parser.add_argument('-circ', help="ROI for the spatial dist.", dest="circ", type=float)
 
 print
 args = parser.parse_args()
@@ -82,39 +82,6 @@ SVec = np.cross(EVec, BVec) / mu0
 t = 1e9*np.linspace(t0,t0+(nots-1)*dt,nots)
 print 'energy flow density = ', 1e6*SVec.sum(axis=0)*dt, " µJ/m²"
 
-# first figure with the time-trace of the fields on axis
-
-left, width = 0.15, 0.80
-rect1 = [left, 0.55, width, 0.40]  #left, bottom, width, height
-rect2 = [left, 0.08, width, 0.40]
-fig = plt.figure(1,figsize=(12,9))
-
-ax1 = fig.add_axes(rect1)
-ax4 = fig.add_axes(rect2, sharex=ax1)
-
-l1 = ax1.plot(t, Ex, "r-", label=r'$E_x$')
-l2 = ax1.plot(t, Ey, "b-", label=r'$E_y$')
-l3 = ax1.plot(t, Ez, "g-", label=r'$E_z$')
-
-ax1.set_ylabel(r'$E$ [V/m]')
-lines = l1 + l2 + l3
-labels = [l.get_label() for l in lines]
-ax1.legend(lines,labels,loc='upper right')
-for label in ax1.get_xticklabels():
-    label.set_visible(False)
-ax1.grid(True)
-
-l4 = ax4.plot(t, Bx, "r-", label=r'$B_x$')
-l5 = ax4.plot(t, By, "b-", label=r'$B_y$')
-l6 = ax4.plot(t, Bz, "g-", label=r'$B_z$')
-
-ax4.set_ylabel(r'$B$ [T]')
-ax4.set_xlabel(r't [ns]')
-lines = l4 + l5 +l6
-labels = [l.get_label() for l in lines]
-ax4.legend(lines,labels,loc='upper right')
-ax4.grid(True)
-
 # second figure with spectrum on axis
 
 nf = nots
@@ -127,8 +94,8 @@ spectB = np.fft.fft(By)[:nots/2]
 # print spectB[:30]
 amplit = np.real(spectE*np.conj(spectB)/mu0)*2*dt/(df*nf)
 
-fig2 = plt.figure(2,figsize=(12,9))
-ax1 = fig2.add_axes([0.1, 0.1, 0.8, 0.8])
+fig1 = plt.figure(1,figsize=(12,9))
+ax1 = fig1.add_axes([0.1, 0.1, 0.8, 0.8])
 l1 = ax1.plot(1e-12*f, 1e6*1e12*amplit, "r-")
 ax1.set_xlabel(r'$f$ [THz]')
 ax1.set_ylabel(r'spectral power density on axis   $dI/dA/df$ [$\mu$J/(m$^2$ THz)]')
@@ -144,7 +111,7 @@ print "FFT max. frequency = %g Hz" % fmax
 print "FFT bin width = %g Hz" % df
 print
 intspec = amplit.sum()*df
-print "integrated spectral power density on axis = %g µJ/m²" % (1e6*intspec)
+print "total spectral power density on axis = %g µJ/m²" % (1e6*intspec)
 
 if roiOK:
   nf1 = np.ceil(f1/df).astype('int')
@@ -170,23 +137,19 @@ for ix in range(Nx):
     totamp += np.real(spectE*np.conj(spectB)/mu0)*2*dt/(df*nf)
 dX = pos[1,0,0]-pos[0,0,0]
 dY = pos[0,1,1]-pos[0,0,1]
+print "dx=%g dy=%g m" % (dX,dY)
 totamp *= dX*dY
 
 ax2 = ax1.twinx()
 l2 = ax2.plot(1e-12*f, 1e6*1e12*totamp, "b-")
-if args.maxf != None:
-    ax2.set_xlim(0.0,1e-12*args.maxf)
+if args.fmax != None:
+    ax2.set_xlim(0.0,1e-12*args.fmax)
 ax2.set_ylabel(r'area integrated spectral power density   $dE/df$ [$\mu$J/THz]')
 ax2.yaxis.label.set_color('b')
 ax2.tick_params(axis='y', colors='b')
-# ax2.legend((l1, l2), (r'$dI/dA/dF$', r'$dI/df$'), 'upper right')
-
-# plt.show()
-# sys.exit()
 
 # third figure with power density on screen
 
-# t = 1e9*np.arange(t0,t0+(nots-1)*dt,dt)
 t = 1e9*np.linspace(t0,t0+(nots-1)*dt,nots)
 X = np.empty([Nx, Ny])
 Y = np.empty([Nx, Ny])
@@ -210,6 +173,7 @@ for ix in range(Nx):
 
 dX = pos[1,0,0]-pos[0,0,0]
 dY = pos[0,1,1]-pos[0,0,1]
+print "dx=%g dy=%g m" % (dX,dY)
 Etot = Pz.sum()*dX*dY
 print "integrated energy = ", 1e6*Etot, " µJ"
 
