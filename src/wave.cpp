d61472a45d8c4881fbce941dfff3b44297634ddb
Ulf Lehnert <ulf@lehnertu.de>
2019-08-06 17:04:36 +0200

    added GaussianWavePacket

--- src/wave.cpp
+++ src/wave.cpp
@@ -101,3 +101,88 @@ ElMagField GaussianWave::LocalField(double t, Vector X)
     return ElMagField(E, B);
 }
 
+
+
+GaussianWavePacket::GaussianWavePacket() :
+    ExternalField()
+{
+    Setup(1.0, complex<double>(0.0, 0.0), 1.0, 1.0, 0.0);
+}
+
+GaussianWavePacket::GaussianWavePacket(Vector pos) :
+    ExternalField(pos)
+{
+    Setup(1.0, complex<double>(0.0, 0.0), 1.0, 1.0, 0.0);
+}
+
+GaussianWavePacket::GaussianWavePacket(const pugi::xml_node node, InputParser *parser) :
+    ExternalField()
+{
+    parser->parseCalcChildren(node);
+    pugi::xml_node position = node.child("position");
+    if (!position)
+        throw(IOexception("InputParser::GaussianWave - wave <position> not found."));
+    else
+    {
+        double x, y, z;
+        x = parser->parseValue(position.attribute("x"));
+        y = parser->parseValue(position.attribute("y"));
+        z = parser->parseValue(position.attribute("z"));
+        origin = Vector(x,y,z);
+    }
+    pugi::xml_node field = node.child("field");
+    if (!field)
+        throw(IOexception("InputParser::GaussianWave - wave <field> not found."));
+    else
+    {
+        double Ar = parser->parseValue(field.attribute("ReA"));
+        double Ai = parser->parseValue(field.attribute("ImA"));
+        double l = parser->parseValue(field.attribute("lambda"));
+        double r = parser->parseValue(field.attribute("rayleigh"));
+        Setup(l, complex<double>(Ar,Ai), r, 1.0, 0.0);
+    }
+}
+
+void GaussianWavePacket::Setup(
+        double lda,
+        complex<double> amplitude,
+        double range,
+        double duration,
+        double arrival
+
+    )
+{
+    lambda = lda;
+    k = 2.0*Pi/lambda;
+    omega = k*SpeedOfLight;
+    A = amplitude;
+    zR = range;
+    w0 = sqrt(lambda*zR/Pi);
+    tau = duration;
+    t0 = arrival;
+    double I0 = norm(A)/2.0*EpsNull*SpeedOfLight;
+    double Ptot = Pi/2.0 * I0*w0*w0;
+    if (teufel::rank==0)
+        std::cout << "gaussian wave packet  E_peak = " << sqrt(norm(A)) << " V/m   w0 = " << w0 << " m   P_peak = " << Ptot << " W" << std::endl;
+}
+
+ElMagField GaussianWavePacket::LocalField(double t, Vector X)
+{
+    // cylindrical coordinates
+    double z = X.z;
+    double r = Vector(X.x,X.y,0.0).norm();
+    // mode size
+    double w = w0 * sqrt(1.0+pow(z/zR,2.0));
+	// curvature radius
+	double R = z + zR*zR/z;
+	// field density fraction
+	// the phase factor is missing because it is contained in the complex amplitude
+	complex<double> dens =
+	    exp(complex<double>(-pow(r/w,2.0)-pow((t-t0)/tau,2.0), omega*t -k*z -Pi*r*r/lambda/R));
+	// std::cout << "z=" << z << " m    r=" << r << " m   ";
+	// std::cout << "fac=(" << dens.real() << ", " << dens.imag() << ")" << std::endl;
+	Vector E = Vector( (A*dens).real(), 0.0, 0.0 );
+	Vector B = Vector(0.0, (A*dens).real()/SpeedOfLight, 0.0);
+    return ElMagField(E, B);
+}
+
--- src/wave.h
+++ src/wave.h
@@ -35,12 +35,12 @@
  * @date 9.4.2019
  * 
  * This class describes a gaussian mode of electromagnetic radiation
- * describing a propagating paraxial wave. The origin of the field is
+ * describing a propagating paraxial wave. The origin of the field object is
  * the mode waist. The propagation direction is along the z-axis and
  * the polarization of the electic field is along the x-axis.
  * Possitions and directions can be changed by transformations
  * given in the constructors applied through the ExternalField base class.
- * The transverse mode seize is defined through the Rayleigh length.
+ * The transverse mode size is defined by the Rayleigh length.
  * 
  * The radiation power is constant all along the beam with the
  * field amplitude varying accordin to the mode size. It is defined through
@@ -85,9 +85,12 @@ public:
      *  - range = 1.0
      */
     void Setup(
-        double lambda,					//! wavelength [m]
-        complex<double> amplitude,		//! the field amplitude at the waist center [V/m]
-        double range					//! the Rayleigh length [m]
+        //! wavelength [m]
+        double lambda,
+        //! the field amplitude at the waist center [V/m]
+        complex<double> amplitude,
+        //! the Rayleigh length [m]
+        double range
         );
 
 	/*! Report the wavelength lambda [m]
@@ -99,12 +102,121 @@ private:
 	/*! compute the electromagnetic fields in local coordinates */
     ElMagField LocalField(double t, Vector X);
 
-    double lambda;						//! wavelength [m]
-    double k;							//! wave number [1/m]
-    double omega;						//! circular frequency [Hz]
-    complex<double> A;					//! the field amplitude at the waist center [V/m]
-    double zR;							//! the Rayleigh length [m]
-    double w0;							//! mode size at waist [m]
+    //! wavelength [m]
+    double lambda;
+    //! wave number [1/m]
+    double k;	
+    //! circular frequency [Hz]						
+    double omega;
+    //! the field amplitude at the waist center [V/m]
+    complex<double> A;
+    //! the Rayleigh length [m]
+    double zR;
+    //! mode size at waist [m]
+    double w0;
+
+};
+
+/*!
+ * \class GaussianWavePacket
+ * \brief A Gaussian electroagnetic wave with Gaussian time dependence
+ * @author Ulf Lehnert
+ * @date 6.8.2019
+ * 
+ * This class describes a gaussian mode of electromagnetic radiation
+ * describing a propagating paraxial wave. The origin of the field object is
+ * the mode waist. The propagation direction is along the z-axis and
+ * the polarization of the electic field is along the x-axis.
+ * Possitions and directions can be changed by transformations
+ * given in the constructors applied through the ExternalField base class.
+ * The transverse mode size is defined by the Rayleigh length.
+ * 
+ * The intensity of the wave has a gaussian time dependence.
+ * For setup the pulse duration is given by the time constant \f$ \tau \f$.
+ * The absolute time at which the center of the pulse passes through
+ * the element origin is given as \f$ t_0 \f$.
+ * 
+ * The radiation power is defined through
+ * the amlitude at the waist given as a complex value of the electric field  strength.
+ * The real part describes the \f$ cos(\omega t) \f$ terms, the imaginary part
+ * the  \f$ sin(\omega t) \f$ terms, thus, defining the phase at the waist.
+ * 
+ * The peak power is \f$ P = \frac{\pi}{2} I_0 \omega_0^2 \f$
+ * with \f$ I_0 = |E_0|^2 / (2 \eta) \f$ the peak intensity on axis.
+ * \f$ \eta = 1 / (\epsilon_0 c) \f$ is the vacuum impedance.
+ */
+class GaussianWavePacket : public ExternalField
+{
+
+public:
+
+    /*! The default contructor just calls the default constructor of the base class
+     *  and initalizes all variables with sane values. This will not yet
+     *  produce any field output as the amplitude is zero.
+     */
+    GaussianWavePacket();
+
+    /*! The default contructor just calls the default constructor of the base class
+     *  and initalizes all variables with sane values. This will not yet
+     *  produce any field output as the amplitude is zero.
+     */
+    GaussianWavePacket(Vector pos);
+
+    /*! This constructor takes the information from an XML node
+     *  describing all wave properties. It will throw exceptions
+     *  if necessary information is missing or cannot be interpreted.
+     * 
+     *  A reference to the input parser must be provided as it is
+     *  necessary to run the input through the calculator.
+     */
+    GaussianWavePacket(const pugi::xml_node node, InputParser *parser);
+
+    /*! Specify the electromagnetic field properties.<br>
+     *  This is called by the constructors with following default values :
+     *  - omega = 1.0
+     *  - A = (0.0, 0.0)
+     *  - range = 1.0
+     *  - tau = 1.0
+     *  - t0 = 0.0
+     */
+    void Setup(
+        //! wavelength [m]
+        double lambda,
+        //! the field amplitude at the waist center [V/m]
+        complex<double> amplitude,
+        //! the Rayleigh length [m]
+        double range,
+        //! pulse length [s]
+        double duration,
+        //! time at waist [s]
+        double arrival
+        );
+
+	/*! Report the wavelength lambda [m]
+	 */
+	double getWavelength() { return lambda; }
+	
+private:
+
+	/*! compute the electromagnetic fields in local coordinates */
+    ElMagField LocalField(double t, Vector X);
+
+    //! wavelength [m]
+    double lambda;
+    //! wave number [1/m]
+    double k;	
+    //! circular frequency [Hz]						
+    double omega;
+    //! the field amplitude at the waist center [V/m]
+    complex<double> A;
+    //! the Rayleigh length [m]
+    double zR;
+    //! mode size at waist [m]
+    double w0;
+    //! pulse length [s]
+    double tau;
+    //! time at waist [s]
+    double t0;
 
 };
 
