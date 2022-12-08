cf4b158ef2f1f17e02ae0bc8bf67ecb9816ea4b0
Ulf Lehnert <u.lehnert@hzdr.de>
2022-12-08 16:05:15 +0100

    extended beam parameter logging and plotting

--- scripts/plot_BeamParameters.py
+++ scripts/plot_BeamParameters.py
@@ -47,21 +47,38 @@ if args.listpar:
 if args.listcol:
     data.listColumns()
 
-if ("t" in data.columnName):
-    t = np.array(data.getColumnData("t"))*1e9
-
 x = np.array(data.getColumnData("x_av"))
+print(f'{len(x)} data points')
 y = np.array(data.getColumnData("y_av"))
 z = np.array(data.getColumnData("z_av"))
+if ("t" in data.columnName):
+    t = np.array(data.getColumnData("t"))*1e9
+    print(f'time span in file {t[0]} ... {t[-1]} s')
 bgx = np.array(data.getColumnData("bgx_av"))
 bgy = np.array(data.getColumnData("bgy_av"))
 bgz = np.array(data.getColumnData("bgz_av"))
+<<<<<<< HEAD
 gamma = np.sqrt(np.square(bgx)+np.square(bgy)+np.square(bgz))
 print(f'gamma[start] = {gamma[0]}')
 print(f'gamma[end] = {gamma[-1]}')
 dgamma = gamma-gamma[0]
 delta = np.array(data.getColumnData("delta"))
 bf = np.array(data.getColumnData("BF"))
+=======
+if ("gamma" in data.columnName):
+    gamma = np.array(data.getColumnData("gamma"))
+    print(f'beam energy gamma start {gamma[0]} end {gamma[-1]} change {gamma[-1]-gamma[0]}')
+else:
+    gamma = np.zeros_like(x);
+if ("delta" in data.columnName):
+    delta = np.array(data.getColumnData("delta"))
+else:
+    delta = np.zeros_like(x);
+if ("BF" in data.columnName):
+    bf = np.array(data.getColumnData("BF"))
+else:
+    bf = np.zeros_like(x);
+>>>>>>> 2a62b80697289029d4c99fa37c97376b7a5abd42
 x_rms = np.array(data.getColumnData("x_rms"))
 y_rms = np.array(data.getColumnData("y_rms"))
 z_rms = np.array(data.getColumnData("z_rms"))
@@ -89,7 +106,11 @@ ax1.grid(True)
 if (args.bunching):
     l4 = ax4.plot(t, delta, "r-", label=r'$\delta$')
     l5 = ax5.plot(t, bf, "b-", label=r'$BF$')
+<<<<<<< HEAD
     l6 = ax4.plot(t,dgamma, "g-", label=r'$\Delta\gamma')
+=======
+    l6 = ax4.plot(t, gamma-gamma[0], "g-", label=r'$\gamma - \gamma[0]$')
+>>>>>>> 2a62b80697289029d4c99fa37c97376b7a5abd42
     ax4.set_ylabel(r'$\delta$')
     ax4.ticklabel_format(useOffset=False)
     for label in ax4.get_xticklabels():
@@ -99,7 +120,11 @@ if (args.bunching):
     ax5.ticklabel_format(useOffset=False)
     for label in ax5.get_xticklabels():
         label.set_visible(False)
+<<<<<<< HEAD
     lines = l4 + l5 + l6
+=======
+    lines = l4 + l5 +l6
+>>>>>>> 2a62b80697289029d4c99fa37c97376b7a5abd42
 else:
     l4 = ax4.plot(t, bgx, "r-", label=r'$\beta_x\gamma$')
     l5 = ax4.plot(t, bgy, "b-", label=r'$\beta_y\gamma$')
